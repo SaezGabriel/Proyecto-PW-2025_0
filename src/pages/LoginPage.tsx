f5d8c8aac4cf9e50e6aa57317bd940cec1a32867
20223522 <20223522@aloe.ulima.edu.pe>
2025-01-25 21:15:27 -0500

    V1

---
+++ src/pages/LoginPage.tsx
@@ -0,0 +1,24 @@
+const LoginPage = () => {
+
+    return  <div className=" row">
+                <div className="col"></div>
+                <div className="col">
+                    <h1>Log In</h1>
+                    <form>
+                        <div>
+                            <input className="form-control" type="text"/>
+                        </div>
+                        <div>
+                            <input className="form-control" type="password"/>
+                        </div>
+                        <button className="btn btn-primary" type="button">Ingresar</button>
+                    </form>
+                </div>
+                <div className="col"></div>
+            </div>
+
+
+
+}
+
+export default LoginPage
