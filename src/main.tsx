0eed4bb01907a856ae2f132222af7840622a6f5c
20223522 <20223522@aloe.ulima.edu.pe>
2025-01-26 01:20:48 -0500

    React comfirmacion_correo y filtrar rol

--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,13 @@ import { createRoot } from 'react-dom/client'
 
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css"
 import LoginPage from './pages/LoginPage'
+import Confirmacion_correo from './pages/Confirmacion_correo'
+import Filtrar_rol from './pages/Filtrar_rol'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <LoginPage />
+    <Confirmacion_correo />
+    <Filtrar_rol />
   </StrictMode>,
 )
