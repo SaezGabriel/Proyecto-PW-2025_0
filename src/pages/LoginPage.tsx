5473784438f41a5607590944a510bf7f8c23d82d
sBenitesF77 <73630608+Yominic@users.noreply.github.com>
2025-01-30 02:41:58 -0500

    Update LoginPage.tsx

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,7 +1,41 @@
-const LoginPage = () => {
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
-   
-    return  <div
+const LoginPage = () => {
+    const navigate = useNavigate();
+    const [usuario, setUsuario] = useState<string>("");
+    const [password, setPassword] = useState<string>("");
+
+    const handleForgotPassword = () => {
+      
+        navigate("/nueva-contraseña");
+    };
+
+    const handleRegister = () => {
+        navigate("/registrar-nuevo");
+    };
+
+    const loginHandler = (usuario : string, password:string) => {
+       
+        if (usuario === "usuario@gmail.com" && password === "123") {
+            navigate("/MainPage_usuario");
+        } else if (usuario === "admin@gmail.com" && password === "123") {
+            navigate("/MainPage_admin");
+        } else {
+            //Incorrecto Login
+        }
+    };
+    const handleUsuarioChange = (e : React.ChangeEvent<HTMLInputElement>) => {
+            setUsuario(e.currentTarget.value)
+    }
+    const handlePasswordChange = (e : React.ChangeEvent<HTMLInputElement>) => {
+        setPassword(e.currentTarget.value)
+    }
+
+    
+ 
+
+ return     <div
             className="d-flex justify-content-center align-items-center bg-body-secondary"
             style={{ height: "100vh" }}
         >
@@ -18,6 +52,8 @@ const LoginPage = () => {
                                 className="form-control"
                                 type="email"
                                 placeholder="Ingresar correo"
+                                value={usuario}
+                                onChange={handleUsuarioChange}
                                 required
                             />
                         </div>
@@ -28,13 +64,15 @@ const LoginPage = () => {
                                 className="form-control"
                                 type="password"
                                 placeholder="Ingresar contraseña"
+                                value={password}
+                                onChange={handlePasswordChange}
                                 required
                             />
                         </div>
                     </div>
                     <div className="row mb-3">
                         <div className="col-12 text-center">
-                            <a href="#" className="text-decoration-underline">
+                            <a href="" onClick={handleForgotPassword} className="text-decoration-underline">
                                 ¿Olvidaste tu contraseña?
                             </a>
                         </div>
@@ -42,7 +80,9 @@ const LoginPage = () => {
                     <div className="row mb-2">
                         <div className="col-12">
                             <div className="d-grid gap-2">
-                                <button className="btn btn-primary" type="submit">
+                                <button  onClick={ () =>{
+                                    loginHandler(usuario,password)
+                                }} className="btn btn-primary" type="submit">
                                     Ingresar
                                 </button>
                             </div>
@@ -56,7 +96,7 @@ const LoginPage = () => {
                     <div className="row mb-2">
                         <div className="col-12">
                             <div className="d-grid gap-2">
-                                <button className="btn btn-secondary" type="button">
+                                <button className="btn btn-secondary" onClick={handleRegister} type="button">
                                     Registrarse
                                 </button>
                             </div>
@@ -67,5 +107,7 @@ const LoginPage = () => {
         </div>
     
 }
+
+export default LoginPage
 
 export default LoginPage
