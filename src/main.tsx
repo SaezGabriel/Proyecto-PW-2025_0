eaaf2b11b139b76f2b17d305fc26b11b7c833d68
sBenitesF77 <73630608+Yominic@users.noreply.github.com>
2025-01-30 02:43:22 -0500

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,29 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import LogIn from "./pages/LoginPage";
+import NuevaContraseña from "./pages/Nueva_contraseña";
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css"
-import LoginPage from './pages/LoginPage'
-import Confirmacion_correo from './pages/Confirmacion_correo'
-import Filtrar_rol from './pages/Filtrar_rol'
+import MainPage_usuario from './pages/MainPage_usuario';
+import Registrar_nuevo from './pages/Registrar_nuevo';
+import MainPage_admin from './pages/MainPage_admin';
+import Confirmacion_correo from './pages/Confirmacion_correo';
+
+
+
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <LoginPage />
-    <Confirmacion_correo />
-    <Filtrar_rol />
+    <Router>
+      <Routes>
+        <Route path="/" element={<LogIn />} />
+        <Route path="/nueva-contraseña" element={<NuevaContraseña />} />
+        <Route path="/confirmacion-correo" element={<Confirmacion_correo />} />
+        <Route path="/registrar-nuevo" element={<Registrar_nuevo />} />
+        <Route path="/mainpage_usuario" element={<MainPage_usuario />} />
+        <Route path="/mainpage_admin" element={<MainPage_admin />} />
+      </Routes>
+    </Router>
   </StrictMode>,
 )
+/*hh*/
