e69cfa99617873b867b1c9031186fbf8a00adc1f
Renzo_Gonzales <20223522@aloe.ulima.edu.pe>
2025-01-31 11:51:50 -0500

    Alarma excesos

--- src/pages/AlarmaExcesoPresupuesto_usuario.tsx
+++ src/pages/AlarmaExcesoPresupuesto_usuario.tsx
@@ -4,17 +4,20 @@ interface AlarmaExcesoPresupuesto_usuario {
 }
 
 const AlarmaPresupuesto = (props : AlarmaExcesoPresupuesto_usuario) => {
-    return <div className={ props.showModal == true ? "modal fade show d-block" : "modal fade"}>
-                <div className="modal-dialog">
+    return <div className={ props.showModal == true ? "modal fade show d-block bg-dark bg-opacity-50" : "modal fade"}>
+                <div className="modal-dialog modal-dialog-centered">
                     <div className="modal-content">
-                        <div className="modal-header">
-                            <h5 className="modal-title" id="exampleModalLabel">¡Alerta!</h5>
-                            <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close" onClick={() => {props.closeModal()}}></button>
-                        </div>
+                            <h5 className="text-center m-4" id="exampleModalLabel">¡Alerta!</h5>
                         <div className="modal-body">
-                                <div className="modal-footer">
-                                    <button type="button" className="btn btn-primary" data-bs-dismiss="modal" onClick={() => {props.closeModal()}} >Aceptar</button>
-                                </div>
+                            <div className="text-justify mb-5 h5 fs-5 m-3 ms-5 ps-4">
+                                Acabas de exeder el presupuesto de servicios
+                            </div>
+                            <div className="d-flex justify-content-center align-items-center mb-3">  
+                                <button type="button" className="btn btn-primary col-4 text-center" onClick={ () =>  {
+                                props.closeModal()
+                                } }>Aceptar</button>
+                            </div>
+                    
                         </div>
                     </div>
                 </div>
